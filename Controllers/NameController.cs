dba497d9abc549c49bd52a56383aff89c34c3e00
RJBrown64 <97131050+RJBrown64@users.noreply.github.com>
2024-10-31 13:33:47 -0400

    added controller

---
+++ Controllers/NameController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCRJB.Controllers
+{
+    [Route("api/name")]
+    [ApiController]
+    public class NameController : Controller
+    {
+        public IActionResult MyName()
+        {
+            var res = new { Name = "RJ BROWN"};
+            return Ok(res);
+        }
+    }
+}
